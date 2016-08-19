b4ba7b44b8c8ff28d4b54795f6dfde048ae41388
Justin Lee <JustinJ.Lee23@gmail.com>
2016-08-19 11:18:48 -0700

    find lines with completion handler syntax via regex

--- Formatter/SourceEditorCommand.swift
+++ Formatter/SourceEditorCommand.swift
@@ -9,10 +9,30 @@
 import Foundation
 import XcodeKit
 
-class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+class SourceEditorCommand: NSObject, XCSourceEditorCommand
+{
     
-    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void {
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: (NSError?) -> Void ) -> Void
+    {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        var updatedLineIndexes = []
+        
+        for lineIndex in 0 ..< invocation.buffer.lines.count
+        {
+            let line = invocation.buffer.lines[lineIndex] as! NSString
+            do
+            {
+                let regex = try RegularExpression(pattern: "\\{.*\\(.+\\).in", options: .caseInsensitive)
+                let range = NSRange(0 ..< line.length)
+                let results = regex.matches(in: line as String, options: .reportProgress, range: range)
+                
+            }
+            catch
+            {
+                completionHandler(error as NSError)
+            }
+        }
         
         completionHandler(nil)
     }
